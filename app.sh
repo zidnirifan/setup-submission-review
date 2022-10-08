63228b724f00f7e337f5c75327b73c2dcce6cd6d
zidnirifan <zidni.rifan7@gmail.com>
2022-10-08 12:19:20 +0700

    fix unresolve directory

--- app.sh
+++ app.sh
@@ -3,20 +3,28 @@
 directory=/home/zidni/Downloads
 folder=review
 
+echo "> Delete directory $directory/$folder"
 rm -rf $directory/$folder
 mkdir $directory/$folder
 
 filename=$(node /home/zidni/projects/setup-submission-review/getFilename.js)
-echo "filename: $filename"
+echo "> Filename: $filename.zip"
 
+echo "> Move $filename.zip to $directory/$folder/"
 mv $directory/$filename.zip $directory/$folder/
 
-unzip $directory/$folder/$filename.zip -d $directory/$folder/$filename
+echo "> Unzip $filename.zip"
+konsole -e unzip $directory/$folder/$filename.zip -d $directory/$folder/$filename
 
 projectFolder=$(node /home/zidni/projects/setup-submission-review/getFolderProject.js)
 
+echo "> Open VSCode on directory: $directory/$folder/$filename"
 code $directory/$folder/$filename
-cp /home/zidni/Documents/env/env $projectFolder/.env
-konsole --workdir $projectFolder -e npm i 
+
+echo "> Copy env to $projectFolder/.env"
+cp /home/zidni/Documents/env/env "$projectFolder/.env"
+
+echo "> Install dependency on folder: $projectFolder"
+konsole --workdir "$projectFolder" -e npm i 
 
 node /home/zidni/projects/setup-submission-review/dropTable.js
